722ce949ab05268d24b5491bf69c58cedc8dde14
Jonathan Shovak <shovakitup@gmail.com>
2022-10-13 13:33:27 -0500

    Made bigBoxDiv, gridDiv, and small box divs by for loops. Big box holds grid, grid holds smallBox divs.

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector("container");
+const bigBoxDiv = document.createElement("div");
+bigBoxDiv.setAttribute("id", "bigBox");
+const gridDiv = document.createElement("div");
+gridDiv.setAttribute("id", "grid");
+let smallBox = null;
+
+const boxWidth = [];
+boxWidth.length = 16;
+
+// this will occur 16 by 16 times 256 times
+for (let n = 0; n < boxWidth.length; n++) {
+  for (let i = 0; i < boxWidth.length; i++) {
+    smallBox = document.createElement("div");
+    gridDiv.appendChild.smallBox;
+    console.log(i);
+  }
+}
